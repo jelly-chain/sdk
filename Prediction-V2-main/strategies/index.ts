6e2f714f659db510ad487e904cae1e6591dd53fe
Jelly <artist.2lane@gmail.com>
2026-05-03 15:26:01 -0700

    Delete Prediction-V2-main/strategies directory

--- Prediction-V2-main/strategies/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './base-strategy.js';
-export * from './momentum-strategy.js';
-export * from './sentiment-strategy.js';
-export * from './event-driven-strategy.js';
-export * from './mean-reversion-strategy.js';
-export * from './volatility-strategy.js';
