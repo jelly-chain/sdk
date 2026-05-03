c384128a9a9908517563435b5bcb4c3dd258bafa
Jelly <artist.2lane@gmail.com>
2026-05-03 15:25:19 -0700

    Delete Prediction-V2-main/triggers directory

--- Prediction-V2-main/triggers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './trigger-engine.js';
-export * from './keyword-trigger.js';
-export * from './event-trigger.js';
-export * from './threshold-trigger.js';
-export * from './composite-trigger.js';
