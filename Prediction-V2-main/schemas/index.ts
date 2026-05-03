2f96651049f870aa423055b224974d3c96a8d81c
Jelly <artist.2lane@gmail.com>
2026-05-03 15:24:23 -0700

    Delete Prediction-V2-main/schemas directory

--- Prediction-V2-main/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './input.schema.js';
-export * from './output.schema.js';
-export * from './event.schema.js';
-export * from './config.schema.js';
-export * from './provider.schema.js';
